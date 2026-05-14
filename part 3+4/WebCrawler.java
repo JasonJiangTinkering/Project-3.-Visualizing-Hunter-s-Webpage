0aef7b26c02a6eaa76d128b739dec2b839f5092e
JasonJiangTinkering <jasonjiangny@gmail.com>
2026-05-14 15:47:49 -0400

    Converted Part 1 + 2 code into a function that can be called, to return a list, with the format of each element as
    Abs Url, Url Name

---
+++ part 3+4/WebCrawler.java
@@ -0,0 +1,81 @@
+// Source: e ListLinks.java sample code from jsoup is
+// https://github.com/jhy/jsoup/blob/master/src/main/java/org/jsoup/examples/ListLinks.java
+
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+// Author: Jason Jiang
+public class WebCrawler {
+
+    // testing main function to test the main features of this webcrawller
+    public static void main(){
+        WebCrawler x = new WebCrawler();
+        String[][] output = x.urlsFromWebSite("www.hunter.cuny.edu");
+        for (i in output){
+            System.out.println("[")
+            for (k in i){
+                System.out.println("\t" + k);
+            }
+            System.out.println("]")
+        }
+    }
+
+// @params: url you are targeting
+// @returns: 2 d list of this format  [[abs url, url name], [....]...]
+// Converted Part 1 + 2 code into a function that can be called, to return a list, with the format of each element as 
+// Abs Url, Url Name
+    public String[][] urlsFromWebSite(String url){
+        print("Fetching %s...", url);
+
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
+
+
+        print("\nLinks: (%d)", links.size());
+        for (Element link : links) {
+            String linkUrl = link.attr("abs:href");
+            String [] allowedPrefixes = {
+                    "https://www.hunter.cuny.edu",
+                    "https://www2.hunter.cuny.edu",
+
+                    "http://www.hunter.cuny.edu",
+                    "https://www2.hunter.cuny.edu",
+
+                    "https://www.hunter.cuny.edu",
+                    "https://www.hunter.cuny.edu",
+                    "https://www.hunter.cuny.edu",
+                    "https://www.hunter.cuny.edu",
+            };
+            // These urls dont have a lot of deviance, so a simple startsWith check is sufficient to filter out the bad urls
+            boolean allowed = false;
+            for (String prefix : allowedPrefixes) {
+                if (linkUrl.startsWith(prefix)) {
+                    allowed = true;
+                    break;
+                }
+            }
+
+            if (!allowed) {
+                // System.out.println("Bad url: " + linkUrl);
+                continue;
+            }
+            print("%s", "./" + linkUrl.substring(linkUrl.indexOf(".edu") + 4), "");
+            // print("%s", linkUrl,"");
+    }
+
+    private static void print(String msg, Object... args) {
+        System.out.println(String.format(msg, args));
+    }
+
+    private static String trim(String s, int width) {
+        if (s.length() > width)
+            return s.substring(0, width-1) + ".";
+        else
+            return s;
+    }
+}
